2173d05520936fa4686efed128ec63fc5a77bd5b
adri <adrimart212@gmail.com>
2025-01-02 13:04:38 +0100

    Created entity, jpa repository, DatabaseInitializer, deleted .sql file and added properties for h2 database

--- src/main/java/com/bcnc/inditexpriceservice/domain/model/Price.java
+++ src/main/java/com/bcnc/inditexpriceservice/domain/model/Price.java
@@ -1,4 +1,48 @@
 package com.bcnc.inditexpriceservice.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "prices")
+
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list", nullable = false)
+    private Long priceList;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "currency", nullable = false)
+    private String currency;
+
 }
--- src/main/java/com/bcnc/inditexpriceservice/infrastructure/config/DatabaseInitializer.java
+++ src/main/java/com/bcnc/inditexpriceservice/infrastructure/config/DatabaseInitializer.java
@@ -1,4 +1,66 @@
 package com.bcnc.inditexpriceservice.infrastructure.config;
 
+import com.bcnc.inditexpriceservice.domain.model.Price;
+import com.bcnc.inditexpriceservice.infrastructure.persistance.PriceRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Component
 public class DatabaseInitializer {
+
+    @Bean
+    public CommandLineRunner loadData(PriceRepository priceRepository) {
+        return inserts -> {
+
+            priceRepository.save(Price.builder()
+                    .brandId(1L)
+                    .startDate(LocalDateTime.of(2020, 6, 14, 0, 0))
+                    .endDate(LocalDateTime.of(2020, 12, 31, 23, 59))
+                    .priceList(1L)
+                    .productId(35455L)
+                    .priority(0)
+                    .price(new BigDecimal("35.50"))
+                    .currency("EUR")
+                    .build());
+
+            priceRepository.save(Price.builder()
+                    .brandId(1L)
+                    .startDate(LocalDateTime.of(2020, 6, 14, 15, 0))
+                    .endDate(LocalDateTime.of(2020, 6, 14, 18, 30))
+                    .priceList(2L)
+                    .productId(35455L)
+                    .priority(1)
+                    .price(new BigDecimal("25.45"))
+                    .currency("EUR")
+                    .build());
+
+            priceRepository.save(Price.builder()
+                    .brandId(1L)
+                    .startDate(LocalDateTime.of(2020, 6, 15, 0, 0))
+                    .endDate(LocalDateTime.of(2020, 6, 15, 11, 0))
+                    .priceList(3L)
+                    .productId(35455L)
+                    .priority(1)
+                    .price(new BigDecimal("30.50"))
+                    .currency("EUR")
+                    .build());
+
+            priceRepository.save(Price.builder()
+                    .brandId(1L)
+                    .startDate(LocalDateTime.of(2020, 6, 15, 16, 0))
+                    .endDate(LocalDateTime.of(2020, 12, 31, 23, 59))
+                    .priceList(4L)
+                    .productId(35455L)
+                    .priority(1)
+                    .price(new BigDecimal("38.95"))
+                    .currency("EUR")
+                    .build());
+
+            System.out.println("Data inserted successfully.");
+        };
+    }
 }
