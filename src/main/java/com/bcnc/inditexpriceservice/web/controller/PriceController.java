ce66264fd2538c15b5a42f7b0fd4b5b748e009df
adri <adrimart212@gmail.com>
2025-01-09 23:54:59 +0100

    Added openapi

--- src/main/java/com/bcnc/inditexpriceservice/web/controller/PriceController.java
+++ src/main/java/com/bcnc/inditexpriceservice/web/controller/PriceController.java
@@ -2,6 +2,8 @@ package com.bcnc.inditexpriceservice.web.controller;
 
 import com.bcnc.inditexpriceservice.application.dto.PriceDTO;
 import com.bcnc.inditexpriceservice.application.service.PriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,11 +20,12 @@ public class PriceController {
         this.priceService = priceService;
     }
 
+    @Operation(summary = "Get applicable price", description = "Fetch the applicable price based on date, product, and brand.")
     @GetMapping
     public PriceDTO getPrice(
-            @RequestParam LocalDateTime applicationDate,
-            @RequestParam Long productId,
-            @RequestParam Long brandId) {
+            @RequestParam @Parameter LocalDateTime applicationDate,
+            @RequestParam @Parameter Long productId,
+            @RequestParam @Parameter Long brandId) {
         return priceService.getApplicablePrice(applicationDate, productId, brandId);
     }
 }
